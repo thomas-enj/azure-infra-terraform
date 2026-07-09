8ab4edaf8b5bc9f2be4494560a5dbeb0914711a1
Thomas Enjalbert <thomas.enjalbert.pro@gmail.com>
2026-07-09 16:46:56 +0200

    feat: Implement virtual network and subnet resources with security group rules

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,13 +1,11 @@
-# TODO : exposer l'ID du VNet et les IDs des subnets (utiles si d'autres modules en ont besoin)
-#
-# output "vnet_id" {
-#   value = azurerm_virtual_network.vnet.id
-# }
-#
-# output "subnet_frontend_id" {
-#   value = azurerm_subnet.frontend.id
-# }
-#
-# output "subnet_backend_id" {
-#   value = azurerm_subnet.backend.id
-# }
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_frontend_id" {
+  value = azurerm_subnet.frontend.id
+}
+
+output "subnet_backend_id" {
+  value = azurerm_subnet.backend.id
+}
